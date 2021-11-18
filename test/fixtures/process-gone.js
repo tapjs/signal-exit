376226223a21b21608cec09d23f7cc0f0b08ccb3
isaacs <i@izs.me>
2021-11-18 08:55:02 -0800

    More properly handle global.process mutating
    The exit handler was set to a no-op whenever the process we grabbed at
    the start didn't match global.process later on.
    
    While this is safe, and fixed the initial failing, it is overly
    conservative, and caused a test in npm/cli to break unnecessarily.  What
    we actually care about is that the process we have is set up with all
    the things we actually care about using.
    
    This commit replaces the `process === global.process` checks with a
    duck-typing function that ensures we won't throw unnecessarily, but
    still allows these methods to be overridden in a new global process
    object.

--- test/fixtures/process-gone.js
+++ test/fixtures/process-gone.js
@@ -1,5 +1,9 @@
-global.process = null
+// have to do this in a hacky way, because removing process right at
+// the start breaks babel, so it fails when coverage is being applied.
 var onSignalExit = require('../../')
+global.process = null
+delete require.cache[require.resolve('../../')]
+onSignalExit = require('../../')
 
 onSignalExit(function (code, signal) {
   throw new Error('this should not ever be called')
